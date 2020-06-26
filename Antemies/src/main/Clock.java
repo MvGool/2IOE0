801c83af5d5862791276de06c5d353fe903afe9b
denisshehu <denisshehu.n@gmail.com>
2020-06-26 10:59:54 +0200

    Fixed a bug in NestObject, updated clock to also change the enemy nest

--- Antemies/src/main/Clock.java
+++ Antemies/src/main/Clock.java
@@ -4,21 +4,26 @@ import main.objects.NestObject;
 
 public class Clock {
 	
-	private NestObject nest;
+	private NestObject userNest;
+	private NestObject enemyNest;
 	private final long ROUND_DURATION = 30000; // 30 seconds
 	private long startTime;
 	private long endTime;
 	
-	public Clock(NestObject nest) {
-		this.nest = nest;
+	public Clock(NestObject userNest, NestObject enemyNest) {
+		this.userNest = userNest;
+		this.enemyNest = enemyNest;
 	}
 	
 	public void run() {
 		while (true) {
 			startTime = System.currentTimeMillis();
-			nest.feedAnts();
-			nest.removeDeadAnts();
-			nest.increasePopulation();
+			userNest.feedAnts();
+			userNest.removeDeadAnts();
+			userNest.increasePopulation();
+			enemyNest.feedAnts();
+			enemyNest.removeDeadAnts();
+			enemyNest.increasePopulation();
 			endTime = System.currentTimeMillis();
 			try {
 				Thread.sleep(ROUND_DURATION - (endTime - startTime));
